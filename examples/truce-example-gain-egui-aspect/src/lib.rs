978d28f83031bb5955c2eb1e99a934914e93243e
Mahae Koh <flomatiks@gmail.com>
2026-06-17 00:24:58 -0700

    CI: wire up truce-example-gain-egui-aspect

--- examples/truce-example-gain-egui-aspect/src/lib.rs
+++ examples/truce-example-gain-egui-aspect/src/lib.rs
@@ -226,4 +226,20 @@ mod tests {
     fn gui_screenshot_macos() {
         truce_test::screenshot!(Plugin, "screenshots/gain_egui_aspect_default_macos.png").run();
     }
+
+    #[cfg(target_os = "linux")]
+    #[test]
+    fn gui_screenshot_linux() {
+        truce_test::screenshot!(Plugin, "screenshots/gain_egui_aspect_default_linux.png")
+            .pixel_threshold(2)
+            .run();
+    }
+
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn gui_screenshot_windows() {
+        truce_test::screenshot!(Plugin, "screenshots/gain_egui_aspect_default_windows.png")
+            .pixel_threshold(2)
+            .run();
+    }
 }
