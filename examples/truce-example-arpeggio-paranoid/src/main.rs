60e5e3ec907ff1d65a13e2542a67c277ff0f99e7
Mahae Koh <flomatiks@gmail.com>
2026-07-06 16:23:45 -0700

    Examples: clean up arpeggio

--- examples/truce-example-arpeggio-paranoid/src/main.rs
+++
@@ -1,5 +0,0 @@
-use truce_example_arpeggio_paranoid::Plugin;
-
-fn main() {
-    truce_standalone::run::<Plugin>();
-}
--- examples/truce-example-arpeggio/src/main.rs
+++ examples/truce-example-arpeggio/src/main.rs
@@ -1,4 +1,4 @@
-use truce_example_arpeggio_simple::Plugin;
+use truce_example_arpeggio::Plugin;
 
 fn main() {
     truce_standalone::run::<Plugin>();
